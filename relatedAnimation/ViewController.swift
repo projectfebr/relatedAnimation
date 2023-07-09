4d09654bccea627212b217d55ee7bc7bceb8dbee
Aleksandr Bolotov <bolotov@siberian.pro>
2023-07-09 15:02:58 +0300

    init

--- relatedAnimation/ViewController.swift
+++ relatedAnimation/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let rect = UIView()
+    private lazy var animator = UIViewPropertyAnimator()
+    private lazy var isTouched = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rect.backgroundColor = .blue
+        rect.layer.cornerRadius = 12
+        rect.frame = CGRect(x: 10, y: 200, width: 100, height: 100)
+        view.addSubview(rect)
+        
+        let slider = UISlider()
+        slider.frame = CGRect(x: 10, y: 350, width: (view.frame.width - 10), height: 50)
+        slider.value = 0
+        view.addSubview(slider)
+        
+        slider.addTarget(self, action: #selector(didChangeValue(_ :)), for: .valueChanged)
+        slider.addTarget(self, action: #selector(didEndEdit(_:)), for: [.touchUpInside, .touchUpOutside])
+        setupAnimator()
     }
-
-
+    
+    @objc
+    private func didChangeValue(_ sender: UISlider) {
+        let value = CGFloat(sender.value)
+        animator.fractionComplete = isTouched ? 1 - value : value
+    }
+    
+    @objc
+    private func didEndEdit(_ sender: UISlider) {
+        animator.isReversed = isTouched
+        
+        animator.addCompletion { _ in
+            self.isTouched = true
+            self.setupAnimator()
+        }
+        
+        animator.startAnimation()
+        sender.setValue(1.0, animated: true)
+    }
+    
+    private func resultFrame() -> CGRect {
+        return isTouched ?
+        CGRect(x: 10, y: 200, width: 100, height: 100) :
+        CGRect(x: (view.frame.width - 150 - 10), y: 200, width: 150, height: 150)
+    }
+    
+    private func setupAnimator() {
+        animator = UIViewPropertyAnimator(duration: 0.7, curve: .easeInOut)
+        
+        animator.addAnimations { [unowned self] in
+            let edge = isTouched ? 0 : (CGFloat.pi / 2)
+            rect.transform = CGAffineTransform(rotationAngle: edge)
+            rect.frame = resultFrame()
+        }
+    }
 }
-
